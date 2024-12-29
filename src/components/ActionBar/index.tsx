d6c88ec2b3be2ca9123e15d83c49d6c6d1c15648
ilia.vatafov <iliyavatafov@gmail.com>
2024-12-29 15:45:19 +0200

    extract some text in constants for ActionBar

--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -1,4 +1,11 @@
-import { FC, HTMLAttributes, useCallback, useState } from "react";
+import {
+  FC,
+  HTMLAttributes,
+  useCallback,
+  useState,
+  useMemo,
+  MouseEvent,
+} from "react";
 
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -6,6 +13,15 @@ import FileCopyIcon from "@mui/icons-material/FileCopy";
 import CheckCircleOutline from "@mui/icons-material/CheckCircleOutline";
 
 import { handleShare as handleShareUtil } from "../../utils/actionBarUtils";
+import {
+  COPY_LINK_KEY,
+  COPY_LINK_TITLE,
+  FACEBOOK_KEY,
+  FACEBOOK_TITLE,
+  TWITTER_KEY,
+  TWITTER_TEXT,
+  TWITTER_TITLE,
+} from "../../constants/actionBar.ts";
 
 import { IconButtonWithTooltip } from "../IconButtonWithTooltip/index.tsx";
 
@@ -14,22 +30,24 @@ interface ActionBarProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const ActionBar: FC<ActionBarProps> = ({ articleUrl, ...rest }) => {
-  const [isFacebookHovered, setIsFacebookHovered] = useState(false);
-  const [isTwitterHovered, setIsTwitterHovered] = useState(false);
-  const [isCopyHovered, setIsCopyHovered] = useState(false);
-  const [isLinkCopied, setIsLinkCopied] = useState(false);
+  const [hoverState, setHoverState] = useState({
+    isFacebookHovered: false,
+    isTwitterHovered: false,
+    isCopyHovered: false,
+    isLinkCopied: false,
+  });
 
   const urlToShare = articleUrl ?? window.location.href;
 
   const handleCopyLink = useCallback(
-    (event: React.MouseEvent) => {
+    (event: MouseEvent) => {
       event.stopPropagation();
 
       navigator.clipboard
         .writeText(urlToShare)
         .then(() => {
           console.log("URL copied to clipboard");
-          setIsLinkCopied(true);
+          setHoverState((prevState) => ({ ...prevState, isLinkCopied: true }));
         })
         .catch((error) => console.error("Error copying to clipboard:", error));
     },
@@ -38,7 +56,7 @@ export const ActionBar: FC<ActionBarProps> = ({ articleUrl, ...rest }) => {
 
   const handleShare = useCallback(
     (
-      event: React.MouseEvent,
+      event: MouseEvent,
       platform: string,
       urlToShare: string,
       text?: string
@@ -48,42 +66,70 @@ export const ActionBar: FC<ActionBarProps> = ({ articleUrl, ...rest }) => {
     [urlToShare]
   );
 
-  const getButtonsConfig = () => [
-    {
-      key: "Facebook",
-      title: "Сподели във Facebook",
-      onClick: (event: React.MouseEvent) =>
-        handleShare(event, "Facebook", urlToShare),
-      onMouseEnter: () => setIsFacebookHovered(true),
-      onMouseLeave: () => setIsFacebookHovered(false),
-      isHovered: isFacebookHovered,
-      icon: <FacebookIcon />,
-    },
-    {
-      key: "Twitter",
-      title: "Сподели в Twitter",
-      onClick: (event: React.MouseEvent) =>
-        handleShare(event, "Twitter", urlToShare, "Check out this link!"),
-      onMouseEnter: () => setIsTwitterHovered(true),
-      onMouseLeave: () => setIsTwitterHovered(false),
-      isHovered: isTwitterHovered,
-      icon: <TwitterIcon />,
-    },
-    {
-      key: "CopyLink",
-      title: "Копирай линк",
-      onClick: handleCopyLink,
-      onMouseEnter: () => setIsCopyHovered(true),
-      onMouseLeave: () => setIsCopyHovered(false),
-      isHovered: isCopyHovered || isLinkCopied,
-      icon: isLinkCopied ? <CheckCircleOutline /> : <FileCopyIcon />,
-    },
-  ];
+  const getButtonsConfig = useMemo(
+    () => [
+      {
+        key: FACEBOOK_KEY,
+        title: FACEBOOK_TITLE,
+        onClick: (event: MouseEvent) =>
+          handleShare(event, FACEBOOK_KEY, urlToShare),
+        onMouseEnter: () =>
+          setHoverState((prevState) => ({
+            ...prevState,
+            isFacebookHovered: true,
+          })),
+        onMouseLeave: () =>
+          setHoverState((prevState) => ({
+            ...prevState,
+            isFacebookHovered: false,
+          })),
+        isHovered: hoverState.isFacebookHovered,
+        icon: <FacebookIcon />,
+      },
+      {
+        key: TWITTER_KEY,
+        title: TWITTER_TITLE,
+        onClick: (event: MouseEvent) =>
+          handleShare(event, TWITTER_KEY, urlToShare, TWITTER_TEXT),
+        onMouseEnter: () =>
+          setHoverState((prevState) => ({
+            ...prevState,
+            isTwitterHovered: true,
+          })),
+        onMouseLeave: () =>
+          setHoverState((prevState) => ({
+            ...prevState,
+            isTwitterHovered: false,
+          })),
+        isHovered: hoverState.isTwitterHovered,
+        icon: <TwitterIcon />,
+      },
+      {
+        key: COPY_LINK_KEY,
+        title: COPY_LINK_TITLE,
+        onClick: handleCopyLink,
+        onMouseEnter: () =>
+          setHoverState((prevState) => ({ ...prevState, isCopyHovered: true })),
+        onMouseLeave: () =>
+          setHoverState((prevState) => ({
+            ...prevState,
+            isCopyHovered: false,
+          })),
+        isHovered: hoverState.isCopyHovered || hoverState.isLinkCopied,
+        icon: hoverState.isLinkCopied ? (
+          <CheckCircleOutline />
+        ) : (
+          <FileCopyIcon />
+        ),
+      },
+    ],
+    [handleShare, handleCopyLink, hoverState, urlToShare]
+  );
 
   return (
     <div className="action-bar" {...rest}>
       <div className="action-bar-icons">
-        {getButtonsConfig().map((button) => (
+        {getButtonsConfig.map((button) => (
           <IconButtonWithTooltip
             key={button.key}
             title={button.title}
---
+++ src/constants/actionBar.ts
@@ -0,0 +1,7 @@
+export const FACEBOOK_TITLE = "Сподели във Facebook";
+export const TWITTER_TITLE = "Сподели в Twitter";
+export const COPY_LINK_TITLE = "Копирай линк";
+export const TWITTER_TEXT = "Check out this link!";
+export const FACEBOOK_KEY = "Facebook";
+export const TWITTER_KEY = "Twitter";
+export const COPY_LINK_KEY = "CopyLink";
