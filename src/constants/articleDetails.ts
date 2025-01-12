281633764f8b17415dd04a2854e183234f445272
ilia.vatafov <iliyavatafov@gmail.com>
2025-01-12 16:41:07 +0200

    remove contant

--- src/constants/articleDetails.ts
+++ src/constants/articleDetails.ts
@@ -1,4 +1,3 @@
-export const ADMIN_EMAIL = "iliyavatafov@gmail.com";
 export const DELETE_MODAL_TITLE = "Изтрий статия";
 export const DELETE_MODAL_MESSAGE =
   "Сигурни ли сте, че желаете да изтриете статията?";
